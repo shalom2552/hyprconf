e33cc8bf34deba76ba50cc1f288d4168b4a353a7
shalom2552 <shalom2552@gmail.com>
2026-05-29 21:13:58 +0300

    fix(B6): use dynamic monitor wallpaper set

--- scripts/set-wallpaper.sh
+++ scripts/set-wallpaper.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
 sleep 3
 WALL=$(ls ~/Pictures/wallpapers/*.{jpg,jpeg,png} 2>/dev/null | shuf -n1)
-awww img --outputs DP-3 "$WALL"
-awww img --outputs HDMI-A-1 "$WALL"
+MONITORS=$(hyprctl monitors -j | jq -r '.[].name')
+
+for MON in $MONITORS; do
+    awww img --outputs "$MON" "$WALL"
+done
