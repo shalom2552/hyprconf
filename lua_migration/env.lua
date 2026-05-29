ee1fbef7931111222983bb8a5080eb428100de87
shalom2552 <shalom2552@gmail.com>
2026-05-29 13:38:53 +0300

    fix(luarc): fix wrong name file and unsupported comment

--- lua_migration/env.lua
+++ lua_migration/env.lua
@@ -11,17 +11,18 @@ hl.env("HYPRCURSOR_SIZE","24")
 hl.env("XDG_CURRENT_DESKTOP","Hyprland")
 hl.env("XDG_SESSION_TYPE","wayland")
 hl.env("XDG_SESSION_DESKTOP","Hyprland")
-hl.env("GDK_BACKEND","wayland","x11","*")
-hl.env(GTK_THEME,"Adwaita:dark")
+hl.env("GDK_BACKEND","wayland,x11,*")
+hl.env("GTK_THEME","Adwaita:dark")
 
 -- Toolkit Settings
-hl.env(QT_QPA_PLATFORM,"wayland;xcb")
+hl.env("QT_QPA_PLATFORM","wayland;xcb")
 hl.env("SDL_VIDEODRIVER","wayland")
 hl.env("CLUTTER_BACKEND","wayland")
 
 -- XDG Base Directories
-hl.env(XDG_CONFIG_HOME,"$HOME/.config")
-hl.env(XDG_CACHE_HOME,"$HOME/.cache")
-hl.env(XDG_DATA_HOME,"$HOME/.local/share")
-hl.env(XDG_STATE_HOME,"$HOME/.local/state")
+local home = os.getenv("HOME")
+hl.env("XDG_CONFIG_HOME", home .. "/.config")
+hl.env("XDG_CACHE_HOME", home .. "/.cache")
+hl.env("XDG_DATA_HOME", home .. "/.local/share")
+hl.env("XDG_STATE_HOME", home .. "/.local/state")
 
